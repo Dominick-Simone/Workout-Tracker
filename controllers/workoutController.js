6906ca734741732050a589d4c8ad5ba7e0502739
Dominick Simone <domsimone123@gmail.com>
2021-10-09 17:44:08 -0400

    meeting assignment requirements

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -20,7 +20,33 @@ module.exports = {
           });
     },
     getWorkouts(req,res) {
-        Workout.find({})
+        Workout.aggregate({
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercise.duration"
+                }
+            }
+        })
         .then((workout) => res.json(workout))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
+    getWorkouts(req,res) {
+        Workout.aggregate({
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercise.duration"
+                }
+            }
+        })
+        .sort({ day: -1 })
+        .limit(7)
+        .then((workout) => res.json(workout))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
     }
 }
